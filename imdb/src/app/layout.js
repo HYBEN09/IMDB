7f3e553101562efe030d3fe415acfac685c1fb75
hyebeen byun <hyebeen2658@naver.com>
2023-06-07 00:07:33 +0900

    feat. 다크모드 구현 - next-theme 설치

--- imdb/src/app/layout.js
+++ imdb/src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Providers from "./Providers";
 import { Header } from "@/components/Header/Header   ";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,11 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko-KR">
       <body className={inter.className}>
-        {/* Header */}
-        <Header />
-        {/* Navbar */}
-        {/* SearchBox */}
-        {children}
+        <Providers>
+          <Header />
+          {/* Navbar */}
+          {/* SearchBox */}
+          {children}
+        </Providers>
       </body>
     </html>
   );
